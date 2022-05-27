b93da27fc755199583302e27f4d8525cd1224716
Scrunch7 <102633219+Scrunch7@users.noreply.github.com>
2022-05-27 14:47:20 +1200

    Update Taehg Humphries 103 Task 4.cpp

--- Taehg Humphries 103 Task 4/Taehg Humphries 103 Task 4/Taehg Humphries 103 Task 4.cpp
+++ Taehg Humphries 103 Task 4/Taehg Humphries 103 Task 4/Taehg Humphries 103 Task 4.cpp
@@ -1,60 +1,95 @@
 #include <iostream>
-#include <string>
 
-void Hangman() {
-    //Hangman
-    std::cout << "   _____ \n";
-    std::cout << "  |     | \n";
-    std::cout << "  |     O \n";
-    std::cout << "  |    \\|/ \n";
-    std::cout << "  |     |  \n";
-    std::cout << "  |    / \\ \n";
-    std::cout << "  | \n";
-    std::cout << "  | \n";
+using std::cout;
+using std::cin;
+using std::string;
+
+
+void Line(int dashes)//declaring function for line
+{
+	for (int i = 0; i < dashes; i++)
+	{
+		cout << "-";
+	}
 }
 
-void FreeMan() {
+void Hangman()//declaring function
+{
+	cout << "   ___    \n";
+	cout << "  |   |   \n";
+	cout << "  |   o   \n";
+	cout << "  |  \\|/ \n";
+	cout << "  |   |   \n";
+	cout << "  |  / \\ \n";
+	cout << "  |       \n";
+	cout << "  |       \n";
+	cout << "-----     \n";
+}
 
-    //Walkman
-    std::cout << " O \n";
-    std::cout << "\\|/ \n";
-    std::cout << " | \n";
-    std::cout << "/ \\ ";
-    std::cout << "\n\n";
+void InvertHangMan()//declaring function
+{
+	cout << "   ___    \n";
+	cout << "  |   |   \n";
+	cout << "  |  \\ / \n";
+	cout << "  |   |   \n";
+	cout << "  |  \\|/ \n";
+	cout << "  |   o   \n";
+	cout << "  |       \n";
+	cout << "-----     \n";
+}
 
+void WalkMan()//declaring function
+{
+	cout << "     o    \n";
+	cout << "    \\|/  \n";
+	cout << "     |    \n";
+	cout << "    / \\  \n";
 }
 
 
-void DeadMan() {
+char Play()
+{
+	char guessLetter, realLetter;//declarung 2 varibles
+	realLetter = 'o';
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> guessLetter;
+		if (guessLetter == realLetter)//checking condition 
+		{
+			cout << "\nYour guess is correct!\n";
+			WalkMan();
+			break;
+		}
+		else
+		{
+			if (i < 2)
 
-    //Deadman
-    std::cout << "   _____ \n";
-    std::cout << "  |     | \n";
-    std::cout << "  |    /|\\ \n";
-    std::cout << "  |     | \n";
-    std::cout << "  |    /|\\ \n";
-    std::cout << "  |     O \n";
-    std::cout << "  | \n";
-    std::cout << "  | \n";
-
-
+				cout << "\nSorry, Try again...\n";
+			else
+			{
+				cout << "\nYour guess is wrong\n";
+				InvertHangMan();
+			}
+		}
+	}
+	return(guessLetter);//returning the value of the guess number
 }
 
-void Base() {
 
-    std::cout << "-------";
-    std::cout << "\n\n";
+int main()
+{
+	int dashes = 67;
+	string answer;
 
+	cout << "\nWelcome to play HangMan. This game is to check your spelling skill.\n";
+
+	Line(dashes);
+	cout << "\n";
+
+	Hangman();
+	cout << "\nGuess the missing letter in Yo_bee: ";
+	answer = Play();//) calling function and Assign the return value of the function
 }
 
 
-int main() {
-   
 
-
-    DeadMan();
-    Base();
-
-    std::cout << "haha dead\n\n";
-
-}
